0c18ac76a70d86a3b971f1060c067aad5d799dce
Ta Duy Khanh <khanhtaduy2k5@gmail.com>
2024-03-26 23:13:44 +0700

    snakever1.0

---
+++ Gallery.cpp
@@ -0,0 +1,47 @@
+
+#include "Gallery.h"
+#include "SDL_utils.h"
+
+#if defined(_WIN64) || defined(_WIN32)
+    #include <SDL_image.h>
+#else
+    #include <SDL2/SDL_image.h>
+#endif
+
+
+
+Gallery::Gallery(SDL_Renderer* renderer_)
+    : renderer(renderer_)
+{
+    loadGamePictures();
+}
+
+Gallery::~Gallery()
+{
+    for (SDL_Texture* texture : pictures)
+        SDL_DestroyTexture(texture);
+}
+
+SDL_Texture* Gallery::loadTexture(std::string path )
+{
+    SDL_Texture* newTexture = NULL;
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if ( loadedSurface == NULL )
+        logSDLError(std::cout, "Unable to load image " + path + " SDL_image Error: " + IMG_GetError());
+    else {
+        newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
+        if( newTexture == NULL )
+            logSDLError(std::cout, "Unable to create texture from " + path + " SDL Error: " + SDL_GetError());
+        SDL_FreeSurface( loadedSurface );
+    }
+    return newTexture;
+}
+
+void Gallery::loadGamePictures()
+{
+    pictures[PIC_CHERRY] = loadTexture("mouse.png");
+    pictures[PIC_SNAKE_VERTICAL] = loadTexture("snakeVertical.png");
+    pictures[PIC_SNAKE_HORIZONTAL] = loadTexture("snakeHorizontal.png");
+    pictures[PIC_SNAKE_HEAD] = loadTexture("snakehead1.png");
+}
+
---
+++ Game.cpp
@@ -0,0 +1,123 @@
+
+#include <vector>
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include "Game.h"
+
+using namespace std;
+Game::Game(int _width, int _height)
+    : width(_width), height(_height),
+	  squares(_height, vector<CellType>(_width, CELL_EMPTY)),
+      snake(*this, Position(_width/2, _height/2)),
+      currentDirection(Direction::RIGHT),
+      status(GAME_RUNNING),
+      score(0)
+{
+	addCherry();
+};
+
+Game::~Game()
+{
+
+}
+
+void Game::snakeMoveTo(Position pos) {
+    if (pos.x < 0 || pos.x >= width || pos.y < 0 || pos.y >= height || squares[pos.y][pos.x] == CELL_SNAKE) {
+        isGameOver();
+        return;
+    }
+    if (squares[pos.y][pos.x] == CELL_CHERRY) {
+        score++;
+        snake.eatCherry();
+        addCherry();
+    } else {
+        setCellType(pos, CELL_SNAKE);
+    }
+}
+
+void Game::snakeLeave(Position position)
+{
+    if (position.x >= 0 && position.x < width && position.y >= 0 && position.y < height) {
+        bool emptyCell = true;
+        for (auto &bodyPart : snake.getPositions()) {
+            if (bodyPart == position) {
+                emptyCell = false;
+                break;
+            }
+        }
+        if (emptyCell) {
+            setCellType(position, CELL_EMPTY);
+        }
+    }
+}
+
+void Game::processUserInput(Direction direction)
+{
+    inputQueue.push(direction);
+}
+
+bool Game::canChange(Direction current, Direction next) const {
+	if (current == UP || current == DOWN) {
+        return (next != UP && next != DOWN);
+    } else {
+        return (next != LEFT && next != RIGHT);
+    }
+}
+
+void Game::nextStep() {
+    while (!inputQueue.empty()) {
+        Direction next = inputQueue.front();
+        inputQueue.pop();
+        if (canChange(currentDirection, next)) {
+            currentDirection = next;
+            break;
+        }
+    }
+    snake.move(currentDirection);
+}
+
+void Game::addCherry() {
+    do {
+        Position randomPos;
+        randomPos.x = rand() % width;
+        randomPos.y = rand() % height;
+        if (getCellType(randomPos) == CELL_EMPTY) {
+
+            cherryPosition = randomPos;
+            setCellType(randomPos, CELL_CHERRY);
+            break;
+        }
+    }
+    while (true);
+}
+
+void Game::setCellType(Position pos, CellType cellType) {
+    squares[pos.y][pos.x] = cellType;
+}
+
+CellType Game::getCellType(Position pos) const
+{
+	return pos.isInsideBox(0, 0, width, height) ? squares[pos.y][pos.x] : CELL_OFF_BOARD;
+}
+
+vector<Position> Game::getSnakePositions() const
+{
+    return snake.getPositions();
+}
+
+GameStatus Game::getGameStatus(){
+	return status;
+}
+
+int Game::getWidth(){
+	return width;
+}
+
+int Game::getHeight(){
+	return height;
+}
+
+Snake Game::getSnake(){
+	return snake;
+}
